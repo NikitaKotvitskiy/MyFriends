7a9b4409a3f7153f9d6c6d72f6a9d8e176884b97
Nikita Kotvitskiy <xkotvi01@vutbr.cz>
2024-06-12 15:03:37 +0200

    chore: ,NET MAUI project was created

---
+++ src/MyFriends.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFriends.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
