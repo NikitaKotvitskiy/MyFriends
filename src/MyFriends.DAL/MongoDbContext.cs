b745adcf8d514b3d824f14126f28c72aa5a65a72
Nikita Kotvitskiy <xkotvi01@vutbr.cz>
2024-06-13 19:19:39 +0200

    feat: DropDatabaseAsync method was added
    Primary usage of this method is test, but, theoretically, it will be possible to implement an ability for user to drop all his data.

--- src/MyFriends.DAL/MongoDbContext.cs
+++ src/MyFriends.DAL/MongoDbContext.cs
@@ -5,13 +5,17 @@ namespace MyFriends.DAL
 {
     public class MongoDbContext
     {
+        private readonly IMongoClient _client;
         private readonly IMongoDatabase _database;
 
         public MongoDbContext(string connectionString, string databaseName)
         {
-            var client = new MongoClient(connectionString);
-            _database = client.GetDatabase(databaseName);
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase(databaseName);
         }
+
+        public async Task DropDatabaseAsync() =>
+            await _client.DropDatabaseAsync(_database.DatabaseNamespace.DatabaseName);
 
         public IMongoCollection<T> GetCollection<T>() where T : IEntity => _database.GetCollection<T>(typeof(T).Name);
     }
