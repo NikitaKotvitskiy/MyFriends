56337f30a63879722f244d787d809bd64977eb5f
Nikita Kotvitskiy <xkotvi01@vutbr.cz>
2024-06-13 19:20:47 +0200

    test: Friend entity tests were added
    Also, the whole test infrastructure for DAL tests was implemented in separated project: MyFriends.DAL.Test

---
+++ src/MyFriends.DAL.Test/TestData.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MyFriends.DAL.Entities;
+using MyFriends.DAL.Repositories;
+using MyFriends.DAL.Test.TestSeeds;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+namespace MyFriends.DAL.Test
+{
+    public static class TestData
+    {
+        public static async Task<MongoDbContext> GetTestContext()
+        {
+            var context = new MongoDbContext("mongodb://localhost:27017", "MyFriendsTestDb");
+            await context.DropDatabaseAsync();
+            FriendEntityTestSeed.Seed(context);
+
+            return context;
+        }
+    }
+}
